a3038f2325d6273eca74af82a3f9fd0de5069b56
tianzhaotju <40144222+tianzhaotju@users.noreply.github.com>
2024-06-12 10:20:34 +0800

    Update run.py

--- UniXCoder/code/run.py
+++ UniXCoder/code/run.py
@@ -572,7 +572,7 @@ def main():
                         help="Batch size per GPU/CPU for evaluation.")
     parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
                         help="Number of updates steps to accumulate before performing a backward/update pass.")
-    parser.add_argument("--learning_rate", default=5e-5, type=float,
+    parser.add_argument("--learning_rate", default=2e-5, type=float,
                         help="The initial learning rate for Adam.")
     parser.add_argument("--weight_decay", default=0.0, type=float,
                         help="Weight deay if we apply some.")
@@ -587,7 +587,7 @@ def main():
 
     parser.add_argument('--seed', type=int, default=42,
                         help="random seed for initialization")
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--epochs', type=int, default=10,
                         help="training epochs")
 
     args = parser.parse_args()

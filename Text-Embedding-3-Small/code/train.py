24c5052b7424440d3b36d1a006776abf7487182f
tianzhaotju <40144222+tianzhaotju@users.noreply.github.com>
2024-06-12 10:19:00 +0800

    Update train.py

--- Text-Embedding-3-Small/code/train.py
+++ Text-Embedding-3-Small/code/train.py
@@ -7,7 +7,7 @@ os.system("python run.py \
     --model_type=text-embedding-3-small \
     --do_train \
     --epoch 10\
-    --learning_rate 1e-4\
+    --learning_rate 2e-5\
     --train_batch_size 64\
     --eval_batch_size 1\
     --codebase_data_file=../../dataset/MutantBench_code_db_java.csv \
